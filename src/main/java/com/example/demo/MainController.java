8b6daecbf8a25b70ae182dbcaa070b44f55df517
HP\Admin <vsadass2004@gmail.com>
2025-09-06 15:10:40 +0530

    test1

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -12,7 +12,7 @@ public class MainController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello()
     {
-        return  ResponseEntity.ok("HELLOW WORLD RESPONSE FROM CUSTOMER SERVICE1");
+        return  ResponseEntity.ok("HELLOW WORLD RESPONSE FROM CUSTOMER SERVICE");
     }
 
 }
