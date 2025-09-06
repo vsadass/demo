09cda79a54df5b481062b5ba40a3d871f340ac09
HP\Admin <vsadass2004@gmail.com>
2025-09-06 09:40:25 +0530

    First git commit in this project

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MainController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello()
+    {
+        return  ResponseEntity.ok("HELLOW WORLD RESPONSE FROM CUSTOMER SERVICE");
+    }
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() throws  Exception {
+		mockMvc.perform(get("/api/v1/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("HELLOW WORLD RESPONSE FROM CUSTOMER SERVICE"));
+
+	}
+
+}
